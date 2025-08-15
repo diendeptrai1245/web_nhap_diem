207e33367c15a570a4bcdc88672190b560114450
laptop_asus <buiducdien.work@gmail.com>
2025-08-15 17:08:13 +0700

    điển đã sửa

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
+from sqlalchemy import func
 import os
 from dotenv import load_dotenv
 from model import db, Student, Subject, Score, User
@@ -42,11 +43,17 @@ def login():
         if user and user.user_password == password:
             session["username"] = user.user_fullname
         
-            return redirect(url_for("score"))
+            return redirect(url_for("function"))
         else:
             return render_template("login.html", error="Wrong email or password!")
 
     return render_template("login.html")
+
+# SIGN OUT
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
 
 # SIGN UP FUNCTON
 @app.route("/register", methods=["GET", "POST"])
@@ -78,20 +85,56 @@ def register():
     
     return render_template("register.html")
   
-@app.route("/search_student", methods=["POST"])
-def search_student():
-    student_id = request.form["student_id"].strip()
-    student = students_scores.get(student_id)
-    if student:
-        return jsonify({"status": "found", "student": student, "id": student_id})
-    else:
-        return jsonify({"status": "not_found"})
+# FUNCTION  
+@app.route("/function")
+def function():
+    return render_template("function.html", username=session.get("username"))
     
-# LOG OUT
-@app.route("/logout")
-def logout():
-    session.pop("username", None)
-    return redirect(url_for("login"))
+# FINDING STUDENT
+@app.route("/find_student", methods=["GET", "POST"])
+def find_student():
+    if request.method == "POST":
+        student_ID = request.form["student_id"]
+        
+        student = Student.query.filter(func.lower(Student.student_id) == student_ID.lower()).first()     
+        
+        if student:
+            session["student_id"] = student.student_id
+            session["student_name"] = student.student_name
+            
+            return redirect(url_for("input_student_score"))
+        else:
+            return render_template("find_student.html", username=session.get("username"), error="Student ID does not exist")
+  
+    return render_template("find_student.html", username=session.get("username"))
+
+# INPUT STUDENT SCORE
+@app.route("/input_student_score", methods=["GET", "POST"])
+def input_student_score():
+    if request.method == "POST":
+        subjects = Subject.query.all()
+        for subject in subjects:
+            attendance = request.form.get(f"attendance_{subject.subject_id}")
+            midterm = request.form.get(f"midterm_{subject.subject_id}")
+            final_exam = request.form.get(f"final_exam_{subject.subject_id}")
+            overall = request.form.get(f"overall_{subject.subject_id}")
+
+            new_score = Score(
+                student_id=session.get("student_id"),
+                student_name=session.get("student_name"),
+                subject_id=subject.subject_id,
+                subject_name=subject.subject_name,
+                attendance=float(attendance),
+                midterm=float(midterm),
+                final_exam=float(final_exam),
+                overall=float(overall)
+            )
+            db.session.add(new_score)
+
+        db.session.commit()
+        return "Scores saved successfully!"
+    
+    return render_template("input_student_score.html", student_id = session.get("student_id"), student_name = session.get("student_name"))
 
 # STUDENT SCORE
 @app.route("/student_score")
@@ -103,9 +146,9 @@ def student_score():
         if student_fullname in Student.student_name and id_student in Student.student_id:
             return  render_template("student_score.html")
         else:
-            return redirect(url_for("score"))
+            return redirect(url_for("logout"))
     
-    return render_template("score.html")
+    return render_template("student_score.html")
 
 
 if __name__ == "__main__":
